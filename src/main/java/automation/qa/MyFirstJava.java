5cb249a154c161d7050d7ea3e62819ead8b7b98e
Iva Mladzhova <iva_mladjova0031@abv.bg>
2022-01-28 08:43:42 +0200

    added some console interactions

--- src/main/java/automation/qa/MyFirstJava.java
+++ src/main/java/automation/qa/MyFirstJava.java
@@ -1,4 +1,22 @@
 package automation.qa;
 
+import java.util.Scanner;
+
 public class MyFirstJava {
+    public static void main(String[] args) {
+        //writes the text in the console
+        System.out.println("Please enter your first name:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+
+        System.out.println("Your name is: " + firstName);
+
+        System.out.println("Your favorite hobby is:");
+
+        int userHobby = scanner.nextInt();
+
+
+    }
 }
