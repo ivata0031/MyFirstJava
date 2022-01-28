955623296676c38aa849f63ba13fadeb1ee1fdda
Iva Mladzhova <iva_mladjova0031@abv.bg>
2022-01-28 09:35:09 +0200

    added some console interactions

--- src/main/java/automation/qa/MyFirstJava.java
+++ src/main/java/automation/qa/MyFirstJava.java
@@ -6,16 +6,15 @@ public class MyFirstJava {
     public static void main(String[] args) {
         //writes the text in the console
         System.out.println("Please enter your first name:");
-
         Scanner scanner = new Scanner(System.in);
-
         String firstName = scanner.next();
 
-        System.out.println("Your name is: " + firstName);
+        System.out.println("Your favorite hobby is: ");
+        String hobby = scanner.next();
 
-        System.out.println("Your favorite hobby is:");
 
-        int userHobby = scanner.nextInt();
+        System.out.println("Your name is: " + firstName );
+        System.out.println("Your favorite hobby is: " + hobby );
 
 
     }
