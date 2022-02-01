30511f9cc636118a3a3f01900b404097470f7aa8
ivata0031 <98076286+ivata0031@users.noreply.github.com>
2022-02-01 19:29:51 +0200

    Update MyFirstJava.java

--- src/main/java/automation/qa/MyFirstJava.java
+++ src/main/java/automation/qa/MyFirstJava.java
@@ -7,10 +7,10 @@ public class MyFirstJava {
         //writes the text in the console
         System.out.println("Please enter your first name:");
         Scanner scanner = new Scanner(System.in);
-        String firstName = scanner.next();
+        String firstName = scanner.nextLine();
 
         System.out.println("Please enter your favorite hobby: ");
-        String hobby = scanner.next();
+        String hobby = scanner.nextLine();
 
 
         System.out.println("Your name is: " + firstName );
